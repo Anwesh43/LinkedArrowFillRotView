9b996eb33ce97aef99ce942987d4715ff8a70531
Anwesh43 <anweshthecool0@gmail.com>
2018-09-25 09:58:53 +0530

    adding arrowfillrotview class and importing libraries

--- arrowfillrotview/src/main/java/com/anwesh/uiprojects/arrowfillrotview/ArrowFillRotView.kt
+++ arrowfillrotview/src/main/java/com/anwesh/uiprojects/arrowfillrotview/ArrowFillRotView.kt
@@ -3,3 +3,31 @@ package com.anwesh.uiprojects.arrowfillrotview
 /**
  * Created by anweshmishra on 25/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+
+class ArrowFillRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

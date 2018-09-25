9d8e265b89686a400730418b1d6e90523ee39bc6
Anwesh43 <anweshthecool0@gmail.com>
2018-09-25 10:52:07 +0530

    adding drawAFRNode canvas extension method

--- arrowfillrotview/src/main/java/com/anwesh/uiprojects/arrowfillrotview/ArrowFillRotView.kt
+++ arrowfillrotview/src/main/java/com/anwesh/uiprojects/arrowfillrotview/ArrowFillRotView.kt
@@ -14,6 +14,37 @@ import android.content.Context
 
 val nodes : Int = 5
 
+fun Canvas.drawAFRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val wSize : Float = gap / 3
+    val hSize : Float = gap / 2
+    paint.strokeWidth = Math.min(w, h) / 80
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.FILL_AND_STROKE
+    paint.color = Color.parseColor("#7B1FA2")
+    save()
+    translate(w/2, gap * i * gap)
+    for (j in 0..1) {
+        val sf : Float = 1f - 2 * j
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - j * 0.5f)) * 2
+        val sc1 : Float = Math.min(0.5f, sc) * 2
+        val sc2 : Float = Math.min(0.5f, Math.max(0f, sc - 0.5f)) * 2
+        save()
+        translate(0f, (w/2 - hSize) * sc2 * sf)
+        rotate(90f * sf * sc1)
+        drawRect(-wSize/2, -hSize, wSize/2, 0f, paint)
+        val path : Path = Path()
+        path.moveTo(-wSize/2, -hSize)
+        path.lineTo(0f, -hSize - wSize)
+        path.lineTo(wSize/2, -hSize)
+        drawPath(path, paint)
+        restore()
+    }
+    restore()
+}
+
 class ArrowFillRotView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
